ff538de348c2c01e4297f83a6ebb0df1cee6b170
Ben Chapman <bcha@bendingspoons.com>
2025-07-31 19:59:55 +0200

    fix: fix small bug

--- src/components/CommentsSection.tsx
+++ src/components/CommentsSection.tsx
@@ -63,14 +63,11 @@ const CommentsSection = async ({ chartId }: CommentsSectionProps) => {
                 {topLevelComment.replies
                   .sort((a, b) => b.votes.length - a.votes.length)
                   .map((reply) => {
-                    const replyVotesAmt = topLevelComment.votes.reduce(
-                      (acc, vote) => {
-                        if (vote.type === "UP") return acc + 1;
-                        if (vote.type === "DOWN") return acc - 1;
-                        return acc;
-                      },
-                      0
-                    );
+                    const replyVotesAmt = reply.votes.reduce((acc, vote) => {
+                      if (vote.type === "UP") return acc + 1;
+                      if (vote.type === "DOWN") return acc - 1;
+                      return acc;
+                    }, 0);
 
                     const replyVote = topLevelComment.votes.find(
                       (vote) => vote.userId === session?.user.id
